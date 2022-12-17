c3abe3a047e2df49cfa3dc081a8d239c53b8794b
GIT-SIK <xeno_s@naver.com>
2022-12-17 23:58:25 +0900

    jsp

---
+++ src/test/java/dev/scheincomp/jsp/JspApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.scheincomp.jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
