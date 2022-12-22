9492acbd2c817de33ee3455658dff480ec36f172
GIT-SIK <xeno_s@naver.com>
2022-12-22 18:04:21 +0900

    MVC - Study 분할로 값과 필요없는 부분 제거

--- src/main/java/dev/scheincomp/jsp/index/IndexDao.java
+++ src/main/java/dev/scheincomp/jsp/index/IndexDao.java
@@ -3,11 +3,8 @@ package dev.scheincomp.jsp.index;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 @Mapper
 public interface IndexDao {
 
     public IndexVO getIndex();
-    public List<IndexVO> getListIndex();
 }
